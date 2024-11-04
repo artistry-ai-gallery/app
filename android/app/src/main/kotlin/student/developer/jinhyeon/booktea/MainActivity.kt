f3c8ec1bf141805fe991a3445dd91c94bdfc65e5
jinhyeon-dev <jinhyeon.dev@gmail.com>
2024-11-04 12:18:50 +0900

    first commit

--- android/app/src/main/kotlin/student/developer/jinhyeon/booktea/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package student.developer.jinhyeon.booktea
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/styles/colors.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const Color primaryColor = Color(0xFF3498DB);
-  static const Color secondaryColor = Color(0xFF2ECC71);
-  static const Color backgroundColor = Color.fromARGB(255, 255, 255, 255);
-  static const Color textColor = Color(0xFF2C3E50);
-}
